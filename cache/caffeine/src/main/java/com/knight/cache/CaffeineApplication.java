c362552ef3e52f389ca22674e261ca6d25ccffaf
TortoiseKnight <yang.liu22@outlook.com>
2022-08-25 00:09:21 +0800

    feat: 新增项目——本地缓存框架

---
+++ cache/caffeine/src/main/java/com/knight/cache/CaffeineApplication.java
@@ -0,0 +1,16 @@
+package com.knight.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author TortoiseKnightB
+ * @date 2022/08/25
+ */
+@SpringBootApplication
+public class CaffeineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaffeineApplication.class, args);
+    }
+}
