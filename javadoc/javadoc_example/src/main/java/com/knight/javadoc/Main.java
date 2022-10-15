e542a100f15f3f252c845417919116948ac1e4aa
TortoiseKnight <yang.liu22@outlook.com>
2022-10-15 11:31:47 +0800

    feat: 项目初始化

---
+++ javadoc/javadoc_example/src/main/java/com/knight/javadoc/Main.java
@@ -0,0 +1,13 @@
+package com.knight.javadoc;
+
+import com.knight.javadoc.controller.TestController;
+
+/**
+ * JavaDoc演示项目
+ *
+ * @author TortoiseKnightB
+ * @date 2022/10/15
+ * @see TestController
+ */
+public class Main {
+}
---
+++ javadoc/javadoc_example/src/main/java/com/knight/javadoc/controller/TestController.java
@@ -0,0 +1,8 @@
+package com.knight.javadoc.controller;
+
+/**
+ * @author TortoiseKnightB
+ * @date 2022/10/15
+ */
+public class TestController {
+}
